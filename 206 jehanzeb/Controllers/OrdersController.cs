44edd738112d6f78ca05e28d272b4d0c4a194639
jehanzeb <jehanzeb@DESKTOP-6QVNI3N>
2022-01-12 00:59:34 +0500

    Add project files.

---
+++ 206 jehanzeb/Controllers/OrdersController.cs
@@ -0,0 +1,158 @@
+﻿using _206_jehanzeb.DataDb;
+using _206_jehanzeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _206_jehanzeb.Controllers
+{
+    public class OrdersController : Controller
+    {
+
+        private readonly StoreDb _context;
+
+        public OrdersController(StoreDb context)
+        {
+            _context = context;
+        }
+
+
+        // GET: Orders
+        public async Task<IActionResult> Index()
+        {
+            return View("~/Views/Invoice/Index.cshtml", await _context.orders.ToListAsync());
+        }
+
+
+        // GET: Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.orders
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            double total = 0;
+            total = order.quantity * order.price;
+            ViewBag.message = total;
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,name,descirption,quantity,price")] OrderModel order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,name,descirption,quantity,price")] OrderModel order)
+        {
+            if (id != order.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.orders
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var order = await _context.orders.FindAsync(id);
+            _context.orders.Remove(order);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.orders.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ 206 jehanzeb/DataDb/StoreDb.cs
@@ -0,0 +1,18 @@
+﻿using _206_jehanzeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _206_jehanzeb.DataDb
+{
+    public class StoreDb : DbContext
+    {
+        public StoreDb(DbContextOptions<StoreDb> options) : base(options)
+        {
+
+        }
+        public DbSet<OrderModel> orders { get; set; }
+    }
+}
